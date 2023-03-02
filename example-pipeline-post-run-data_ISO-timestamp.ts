8a90df67bf47efd09032e6b0b3e55d987ed8c8b7
Brad M Mallow <briznad@gmail.com>
2023-03-02 14:34:21 -0800

    add concept of which action invokes another action

--- example-pipeline-post-run-data_ISO-timestamp.ts
+++ example-pipeline-post-run-data_ISO-timestamp.ts
@@ -1,4 +1,5 @@
-type Action = {
+type PipelineOrAction = {
+	id: string;
 	title: string;
 	startedAt: string; // ISO date string
 	endedAt: string; // ISO date string
@@ -6,12 +7,17 @@ type Action = {
 	success: boolean;
 };
 
-interface PipelinePostRunData extends Action {
+interface Action extends PipelineOrAction {
+	spawnedBy? : string; // id of preceeding action that invoked this action; omitted for first action
+}
+
+interface PipelinePostRunData extends PipelineOrAction {
 	actions: Action[];
 }
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 const examplePostRunData : PipelinePostRunData = {
+	id: 'n540Mrgydc',
 	title: 'Pantry Build & Deploy',
 	startedAt: '2023-03-02T00:53:11.606Z',
 	endedAt: '2023-03-02T01:23:10.302Z',
@@ -19,6 +25,7 @@ const examplePostRunData : PipelinePostRunData = {
 	success: true,
 	actions: [
 		{
+			id: 'ZG5wtVoiAo',
 			title: 'Build',
 			startedAt: '2023-03-02T00:53:11.606Z',
 			endedAt: '2023-03-02T01:23:10.302Z',
@@ -26,11 +33,13 @@ const examplePostRunData : PipelinePostRunData = {
 			success: true,
 		},
 		{
+			id: 'wbgRQlCurX',
 			title: 'Deploy',
 			startedAt: '2023-03-02T00:53:11.606Z',
 			endedAt: '2023-03-02T01:23:10.302Z',
 			elapsedSeconds: 44,
 			success: true,
+			spawnedBy: 'ZG5wtVoiAo',
 		},
 	],
 };

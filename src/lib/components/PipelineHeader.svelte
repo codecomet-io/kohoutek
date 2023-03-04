9f02b2a56c3f9c5e14a6b4a08a831a95bbe3eabb
Brad Mallow <briznad@gmail.com>
2023-03-03 16:14:48 -0800

    add logo/favicon

--- src/lib/components/PipelineHeader.svelte
+++ src/lib/components/PipelineHeader.svelte
@@ -34,11 +34,34 @@
 		margin-top: 7px;
 		margin-bottom: 0;
 	}
+
+	.corp-site-link {
+		display: flex;
+		height: 2rem;
+		align-items: center;
+		color: #1c1e21;
+		text-decoration: none;
+		font-family: system-ui,-apple-system,Segoe UI,Roboto,Ubuntu,Cantarell,Noto Sans,sans-serif,BlinkMacSystemFont,"Segoe UI",Helvetica,Arial,sans-serif,"Apple Color Emoji","Segoe UI Emoji","Segoe UI Symbol";
+
+		&:hover {
+			color: #5468ff;
+
+			img {
+				transform: translateX(-2px) translateY(2px) scale(110%);
+			}
+		}
+
+		img {
+			height: 100%;
+			margin-right: 0.25rem;
+			transition: transform 125ms ease-in-out;
+		}
+	}
 </style>
 
 
 <ion-header translucent={ true }>
-  <ion-toolbar class="">
+  <ion-toolbar>
 		<div class="title-container">
 			<StatusIcon
 				status={ pipeline.status }
@@ -51,5 +74,21 @@
 		<div class="subtitle">
 			<ChunkyLabel>{ pipeline.status === 'success' ? 'succeed' : 'fail' }ed <Ago date={ pipeline.endedAt } /> <span title="{ pipeline.elapsedSeconds } seconds">in { parseLapsed(pipeline.elapsedSeconds * 1000) }</span></ChunkyLabel>
 		</div>
+
+		<a
+			slot="end"
+			class="corp-site-link"
+			href="https://codecomet.io/"
+			itemtype="http://schema.org/Corporation"
+			itemscope
+		>
+			<img
+				src="/CodeComet-logo.svg"
+				alt="CodeComet logo showing an illustrated comet entering the atmosphere"
+				itemprop="image"
+			/>
+
+			<strong itemprop="name">CodeComet</strong>
+		</a>
   </ion-toolbar>
 </ion-header>

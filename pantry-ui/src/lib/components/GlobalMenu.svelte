a7117b0292249311572f8d9858edcd6ecf9c215e
Brad Mallow <briznad@gmail.com>
2023-05-12 16:04:58 -0700

    init

---
+++ pantry-ui/src/lib/components/GlobalMenu.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+	import { close } from 'ionicons/icons';
+
+	import CodeCometLogo from '$lib/components/CodeCometLogo.svelte';
+</script>
+
+
+<style lang="scss"></style>
+
+
+<ion-header>
+	<ion-toolbar>
+		<CodeCometLogo />
+
+		<ion-buttons slot="end">
+			<ion-menu-toggle>
+				<ion-button>
+					<ion-icon
+						slot="icon-only"
+						icon={ close }
+					></ion-icon>
+				</ion-button>
+			</ion-menu-toggle>
+		</ion-buttons>
+	</ion-toolbar>
+</ion-header>
+
+<ion-content class="ion-padding">Menu Content</ion-content>

63c5e019fc464174e5a156c9276485ea3ec9a13d
Brad Mallow <briznad@gmail.com>
2023-05-19 16:07:03 -0700

    add example run link to homepage

--- app/src/routes/+page.svelte
+++ app/src/routes/+page.svelte
@@ -12,4 +12,6 @@
 	<h1>Kohoutek</h1>
 
 	<iframe width="560" height="315" src="https://www.youtube-nocookie.com/embed/ZZ-4rpmepXA" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+
+	<a href="/run/4f7m7bhk">example run</a>
 </div>

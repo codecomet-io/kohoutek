3799a28a9fdc993685dc715810165e1e11ac3450
Brad Mallow <briznad@gmail.com>
2023-05-19 15:08:02 -0700

    attempt to use netlify adapter

--- app/svelte.config.js
+++ app/svelte.config.js
@@ -1,45 +1,13 @@
-import adapter from '@sveltejs/adapter-static'
+import netlifyAdapter from '@sveltejs/adapter-netlify';
 import preprocess from "svelte-preprocess";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	preprocess: preprocess(),
+	preprocess : preprocess(),
 
-	kit: {
-		adapter: adapter({
-			pages: 'build',
-			assets: 'build',
-			fallback: 'index.html',
-			precompress: false
-		})
-	}
+	kit : {
+		adapter : netlifyAdapter({}),
+	},
 };
 
 export default config;
-
-
-
-
-// import multiAdapter from '@macfja/svelte-multi-adapter'
-// import staticAdapter from '@sveltejs/adapter-static';
-// import vercelAdapter from '@sveltejs/adapter-vercel';
-// import preprocess from 'svelte-preprocess';
-
-
-// /** @type {import('@sveltejs/kit').Config} */
-// const config = {
-// 	preprocess: preprocess(),
-
-// 	kit : {
-// 		adapter : multiAdapter([
-// 			staticAdapter({
-// 				pages : 'dist',
-// 				assets : 'dist',
-// 				fallback : 'index.html',
-// 			}),
-// 			vercelAdapter({}),
-// 		]),
-// 	}
-// };
-
-// export default config;

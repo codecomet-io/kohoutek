e6a37ad7b74238043fe0d2768667f0deec0f50cc
spacedub <spaceraccoon@codecomet.io>
2023-05-25 17:56:04 -0700

    Preliminary work for JWT verification

--- app/src/lib/verify-authentication.server.ts
+++ app/src/lib/verify-authentication.server.ts
@@ -1,6 +1,7 @@
 import type { Cookies } from '@sveltejs/kit';
 
 import * as jose from 'jose';
+import type {JWTVerifyResult} from "jose";
 
 
 export type GitHubUser = {
@@ -10,6 +11,30 @@ export type GitHubUser = {
 	personalAccessToken : string;
 	repos?              : string[];
 };
+
+// This MUST be put into some form of configuration - this one is the public key for the local dev server
+const pubKey = `-----BEGIN PUBLIC KEY-----
+MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAxl8JsSmJk1a2oAsbtSzp
+Ci66GgNsIfGDZ7MykrUJSr7i2W4g33mgT/rXW7KdoVsrJJslMMtOgTsKA/PpT59P
+JkF3/WCWjSD5BwGFBqWX9LfVeBF3BP/d8z9RGMfbIqS9Px785PZSr1rsQUezH8tr
+z8IioML7yRcvGIPCjY8GLLXNYHPsWLjfmYqfZC6kTapFj9yKjSu2LuqRDNjDKFCH
+j8KBTVyFjYV9EsAFs4ndJqf/IcDqVchyxivXq53IvVpAfA3ig6EQzJDK1RrLskAL
+sThoXq1j1m3ha2GZwXozEA5miziA6zboNy+sZlBpyX56bq0DHQpjlXw6GLtQqNNA
+eQIDAQAB
+-----END PUBLIC KEY-----`;
+
+// And this one is the staging one:
+/*
+const pubKey = `-----BEGIN PUBLIC KEY-----
+MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAyWStRb9i+hGWb6ZrCc1l
+3DOXIwpyNGIss2yUtAt/42EqWeuPZTNNztXe5ZlWJH6jYsFREU9IsbQI+6itNqRi
+44I2tJat1r6OXse4OwroTSmNG4CluoVQkNgdJa3LoJyHOlAnNQoeroYKKKdwSd9p
+oq6N7Mh4E6yZHxkcS7nvK5DoKon39u8RklFTCGb7Rlwiz4gReV1wHunHSCUA9sWh
+vi6d/vip0P2yQ2s8/yKAeyjfb2X9kLLzL1kWU9cWB7zbqKG9wRcdMLpNzu1mTylz
+4thAZ15UaZeKldlI47kmEMcp74Hebpkhknd3l79yF83+Ngelj6rQmIYdfVAZOpzh
+vQIDAQAB
+-----END PUBLIC KEY-----`
+ */
 
 
 export class VerifyAuthentication {
@@ -44,6 +69,25 @@ export class VerifyAuthentication {
 		}
 
 		this.decodedToken = decoded;
+	}
+
+	private async verifySignature(token: string): Promise<JWTVerifyResult> {
+		// Keeping this around for later, to verify what happens with expired tokens
+		// AUTHP_SESSION_ID=IHZ8uoesoib5v7VcF4CLaLGXtAkIKSMxGJrY742ruDUe1; access_token=eyJhbGciOiJSUzUxMiIsInR5cCI6IkpXVCJ9.eyJhZGRyIjoiMTI3LjAuMC4xIiwiZXhwIjoxNjg1MDYyNjUyLCJmb3J3YXJkZWRfdG9rZW4iOiJnaHVfelBRMXRleUUyUEZ1d2c3Vk1wc1BzM1NDcFRXeGpPM3p1T21SIiwiaWF0IjoxNjg1MDU5MDUyLCJpc3MiOiJodHRwczovL2F1dGguY29kZWNvbWV0LmxvY2FsL2F1dGgvb2F1dGgyL2dpdGh1Yi8iLCJqdGkiOiJJSFo4dW9lc29pYjV2N1ZjRjRDTGFMR1h0QWtJS1NNeEdKclk3NDJydURVZTEiLCJtZXRhZGF0YSI6eyJpZCI6MTA0OTU0MjUxfSwibmFtZSI6IlNwYWNlIFJhY2Nvb24iLCJuYmYiOjE2ODUwNTg5OTIsIm9yaWdpbiI6Imh0dHBzOi8vYXBpLmdpdGh1Yi5jb20vdXNlciIsInBpY3R1cmUiOiJodHRwczovL2F2YXRhcnMuZ2l0aHVidXNlcmNvbnRlbnQuY29tL3UvMTA0OTU0MjUxP3Y9NCIsInJlYWxtIjoiZ2l0aHViIiwicm9sZXMiOlsiYXV0aHAvdXNlciIsImF1dGhwL2FkbWluIl0sInN1YiI6ImdpdGh1Yi5jb20vc3BhY2VkdWIifQ.TXIVey__EBTfTnGiTkuPSWlbes0jo-Wyu3ABWuQeV_hzmcM5Xx2MyPG5wGquCQkFM5SoulRQ6Qmjv7QKA-8JQ3VLJIqc84HAU4koBZnT3u1dzBcN1Maen_LwAuMeFY_0tdqSxHhvXfUPBDgJLf_sBZEBoV-Otk2hC1s1DQY5P5X-YM8yY4CNHWJXt45Ft20BnAa0GSHG9BKHz_ADjnMaqhE0mR3Y3mcpKT1OF1zcpvI_WbhZVFcZebrdhsN-vG7hTy0VjjsAI6SEsFCSirvXAoXidWBxvVMqzcRe2HitNdv1PhW7bQkN_YQQj-ZpvTBKoCVbDtikN6Y0VkzYJmEHoQ
+		// document.cookie = "AUTHP_SESSION_ID=IHZ8uoesoib5v7VcF4CLaLGXtAkIKSMxGJrY742ruDUe1; path=/;";
+		// document.cookie = "access_token=eyJhbGciOiJSUzUxMiIsInR5cCI6IkpXVCJ9.eyJhZGRyIjoiMTI3LjAuMC4xIiwiZXhwIjoxNjg1MDYyNjUyLCJmb3J3YXJkZWRfdG9rZW4iOiJnaHVfelBRMXRleUUyUEZ1d2c3Vk1wc1BzM1NDcFRXeGpPM3p1T21SIiwiaWF0IjoxNjg1MDU5MDUyLCJpc3MiOiJodHRwczovL2F1dGguY29kZWNvbWV0LmxvY2FsL2F1dGgvb2F1dGgyL2dpdGh1Yi8iLCJqdGkiOiJJSFo4dW9lc29pYjV2N1ZjRjRDTGFMR1h0QWtJS1NNeEdKclk3NDJydURVZTEiLCJtZXRhZGF0YSI6eyJpZCI6MTA0OTU0MjUxfSwibmFtZSI6IlNwYWNlIFJhY2Nvb24iLCJuYmYiOjE2ODUwNTg5OTIsIm9yaWdpbiI6Imh0dHBzOi8vYXBpLmdpdGh1Yi5jb20vdXNlciIsInBpY3R1cmUiOiJodHRwczovL2F2YXRhcnMuZ2l0aHVidXNlcmNvbnRlbnQuY29tL3UvMTA0OTU0MjUxP3Y9NCIsInJlYWxtIjoiZ2l0aHViIiwicm9sZXMiOlsiYXV0aHAvdXNlciIsImF1dGhwL2FkbWluIl0sInN1YiI6ImdpdGh1Yi5jb20vc3BhY2VkdWIifQ.TXIVey__EBTfTnGiTkuPSWlbes0jo-Wyu3ABWuQeV_hzmcM5Xx2MyPG5wGquCQkFM5SoulRQ6Qmjv7QKA-8JQ3VLJIqc84HAU4koBZnT3u1dzBcN1Maen_LwAuMeFY_0tdqSxHhvXfUPBDgJLf_sBZEBoV-Otk2hC1s1DQY5P5X-YM8yY4CNHWJXt45Ft20BnAa0GSHG9BKHz_ADjnMaqhE0mR3Y3mcpKT1OF1zcpvI_WbhZVFcZebrdhsN-vG7hTy0VjjsAI6SEsFCSirvXAoXidWBxvVMqzcRe2HitNdv1PhW7bQkN_YQQj-ZpvTBKoCVbDtikN6Y0VkzYJmEHoQ; path=/;";
+
+		// Or overriding the token value directly here
+		// token = "eyJhbGciOiJSUzUxMiIsInR5cCI6IkpXVCJ9.eyJhZGRyIjoiMTI3LjAuMC4xIiwiZXhwIjoxNjg1MDYyNjUyLCJmb3J3YXJkZWRfdG9rZW4iOiJnaHVfelBRMXRleUUyUEZ1d2c3Vk1wc1BzM1NDcFRXeGpPM3p1T21SIiwiaWF0IjoxNjg1MDU5MDUyLCJpc3MiOiJodHRwczovL2F1dGguY29kZWNvbWV0LmxvY2FsL2F1dGgvb2F1dGgyL2dpdGh1Yi8iLCJqdGkiOiJJSFo4dW9lc29pYjV2N1ZjRjRDTGFMR1h0QWtJS1NNeEdKclk3NDJydURVZTEiLCJtZXRhZGF0YSI6eyJpZCI6MTA0OTU0MjUxfSwibmFtZSI6IlNwYWNlIFJhY2Nvb24iLCJuYmYiOjE2ODUwNTg5OTIsIm9yaWdpbiI6Imh0dHBzOi8vYXBpLmdpdGh1Yi5jb20vdXNlciIsInBpY3R1cmUiOiJodHRwczovL2F2YXRhcnMuZ2l0aHVidXNlcmNvbnRlbnQuY29tL3UvMTA0OTU0MjUxP3Y9NCIsInJlYWxtIjoiZ2l0aHViIiwicm9sZXMiOlsiYXV0aHAvdXNlciIsImF1dGhwL2FkbWluIl0sInN1YiI6ImdpdGh1Yi5jb20vc3BhY2VkdWIifQ.TXIVey__EBTfTnGiTkuPSWlbes0jo-Wyu3ABWuQeV_hzmcM5Xx2MyPG5wGquCQkFM5SoulRQ6Qmjv7QKA-8JQ3VLJIqc84HAU4koBZnT3u1dzBcN1Maen_LwAuMeFY_0tdqSxHhvXfUPBDgJLf_sBZEBoV-Otk2hC1s1DQY5P5X-YM8yY4CNHWJXt45Ft20BnAa0GSHG9BKHz_ADjnMaqhE0mR3Y3mcpKT1OF1zcpvI_WbhZVFcZebrdhsN-vG7hTy0VjjsAI6SEsFCSirvXAoXidWBxvVMqzcRe2HitNdv1PhW7bQkN_YQQj-ZpvTBKoCVbDtikN6Y0VkzYJmEHoQ"
+
+		// Loading the public key should only happen once in the lifecycle of the app
+		const algorithm = 'RS512';
+		const ecPublicKey = await jose.importSPKI(pubKey, algorithm);
+
+		// Should be called everytime you read a new token, to ensure it is fine
+		const result = await jose.jwtVerify(token, ecPublicKey);
+
+		return result;
 	}
 
 	private authFailed(message : string, callback : (msg : string) => void) {

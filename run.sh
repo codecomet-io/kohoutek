cad0d098d7f1395e98d497a0f0ed9d1557718342
Brad Mallow <briznad@gmail.com>
2023-06-30 12:22:19 -0700

    conditionally run npm install from run.sh

--- run.sh
+++ run.sh
@@ -3,7 +3,29 @@ normalizedSource="$(printf "%s" "$source" | tr "/" "-")"
 normalizedSource="${normalizedSource:-c-failure}"
 
 cd ./pantry/
+
+if [ ! -d "node_modules" ]
+then
+	echo "\"node_modules\" directory doesn't exist. Running npm install."
+
+	npm install
+else
+	echo "\"node_modules\" directory exists. Skipping npm install."
+fi
+
 npm run build "$source"
+
 cd ../app/
+
+if [ ! -d "node_modules" ]
+then
+	echo "\"node_modules\" directory doesn't exist. Running npm install."
+
+	npm install
+else
+	echo "\"node_modules\" directory exists. Skipping npm install."
+fi
+
 npm run build
+
 npm run preview -- --open /anonymous-run/$normalizedSource
